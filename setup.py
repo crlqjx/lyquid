4c020410b715ba10788f50f593b8b40251dd1153
crlqjx <quijouxcyril@outlook.com>
2021-01-04 16:57:57 +0100

    go prod

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="lyquid-crlqjx",
+    name="lyquid",
     version="0.0.1",
     author="Cyril Quijoux",
     author_email="",
